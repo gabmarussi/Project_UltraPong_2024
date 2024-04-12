8726c95eb7f0f6f6df3bec3b435dbe0a08f7ce88
Gabriel Marussi <166125192+gabmarussi@users.noreply.github.com>
2024-04-12 09:47:35 -0300

    Projeto no modo final
    Removido erros, adicionado pontos e alterado textura.

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -1,27 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using TMPro;
 using UnityEngine;
 
-[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
 public class Bola : MonoBehaviour
 {
     public float velocidade = 10;
+
+    public TMP_Text pontuacaoEsquerda;
+    public TMP_Text pontuacaoDireita;
+
+    int pontosEsquerda = 0;
+    int pontosDireita = 0;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
         float x = Random.Range(0,2) == 0 ? -1:1;
-        /*float x1 = Random.Range(0,2);
-        if(x1==0){
-            x1=-1;
-        }else
-        {
-            x1=1;
-        }*/
+       
         float y = Random.Range(0,2) == 0 ? -1:1;
         GetComponent<Rigidbody>().velocity = new Vector2(velocidade*x,velocidade*y);
         
+        pontuacaoEsquerda.SetText(pontosEsquerda.ToString());
+        pontuacaoDireita.SetText(pontosDireita.ToString());
     }
 
     // Update is called once per frame
@@ -30,18 +31,20 @@ public class Bola : MonoBehaviour
       
     }
 
-    /*
+
     void OnCollisionEnter(Collision batida)
     {
-        if ((batida.gameObject.name=="BEsquerda") || (batida.gameObject.name=="BDireita"))
-        
-        transform.position = new Vector3(0, 1, 9);
-        Start();
-
-    }
-    */
-    private string GetDebuggerDisplay()
-    {
-        return ToString();
+        if (batida.gameObject.name == "BEsquerda")
+        {
+            transform.position = new Vector3(0, 1, 9);
+            pontosDireita++;
+            Start();
+        }
+        else if (batida.gameObject.name == "BDireita")
+        {
+            transform.position = new Vector3(0, 1, 9);
+            pontosEsquerda++;
+            Start();
+        }
     }
 }
